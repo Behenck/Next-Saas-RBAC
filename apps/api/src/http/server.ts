88bc341e94383d66d77ab98c3e3f4f3dbb285a52
Behenck <denilsontrespa10@gmail.com>
2024-09-01 19:37:57 -0300

    Next Saas RBAC

---
+++ apps/api/src/http/server.ts
@@ -0,0 +1,57 @@
+import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+import { errorHandler } from './error-handler'
+import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
+import { createAccount } from './routes/auth/create-account'
+import { getProfile } from './routes/auth/get-profile'
+import { requestPassowrdRecover } from './routes/auth/request-password-recover'
+import { resetPassword } from './routes/auth/reset-password'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.setErrorHandler(errorHandler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js Saas',
+      description: 'Full-stack Saas app with multi-tenant & RBAC.',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(fastifyJwt, {
+  secret: 'my-jwt-secret',
+})
+
+app.register(fastifyCors)
+
+app.register(createAccount)
+app.register(authenticateWithPassword)
+app.register(getProfile)
+app.register(requestPassowrdRecover)
+app.register(resetPassword)
+
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+  console.log('HTTP server running!')
+})
---
+++ apps/api/src/index.ts
@@ -0,0 +1,3 @@
+import { definedAbilityFor } from '@saas/auth'
+
+const ability = definedAbilityFor({ role: 'ADMIN' })
